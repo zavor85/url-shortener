7976db5a43432ba6fe5df5a944697aca94b19382
zavor85 <zavor85@gmail.com>
2020-11-05 16:04:27 +0200

    push proj

---
+++ ShortURL/App/URLResponse.cs
@@ -0,0 +1,9 @@
+﻿namespace ShortURL
+{
+    public class URLResponse : IURLResponse
+    {
+        public string Url { get; set; }
+        public string Status { get; set; }
+        public string Token { get; set; }
+    }
+}
